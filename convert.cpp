9f2a51152c02618203429623aad7c35a8d3f6e7f
Andrew Clayton <126101983+andrew-clayton@users.noreply.github.com>
2023-10-16 20:40:15 -0500

    updating conver.tcpp

--- convert.cpp
+++ convert.cpp
@@ -1,9 +1,25 @@
+/**
+ * @file convert.cpp
+ * @brief Convert CSV data into length-indicated format.
+ * @author Andrew Clayton
+ */
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 #include <iomanip>
 
+/**
+ * @brief Converts a given string to a length-indicated format.
+ *
+ * If the length of the provided string is more than 99 characters, 
+ * an error is output and the program exits. Otherwise, the string is 
+ * prepended with its 2-digit length.
+ *
+ * @param line String to be converted.
+ * @return Length-indicated formatted string.
+ */
 std::string convertToLengthIndicated(const std::string &line) {
     // Calculate the total length of the string excluding the length field
     int totalLength = line.size();
@@ -21,6 +37,14 @@ std::string convertToLengthIndicated(const std::string &line) {
     return lengthStream.str() + line;
 }
 
+/**
+ * @brief Main function to process the CSV data.
+ *
+ * Reads from "us_postal_codes_ROWS_RANDOMIZED.csv", converts each line to 
+ * length-indicated format and writes the result to "LI_R.csv".
+ *
+ * @return Returns 0 on success, 1 on file open error.
+ */
 int main() {
     std::ifstream inFile("us_postal_codes_ROWS_RANDOMIZED.csv");
     std::ofstream outFile("LI_R.csv");
